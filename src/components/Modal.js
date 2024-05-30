9b861f47609f0cc646faee62284ab13dad8603b4
rohankokkula <rohankokkula01@gmail.com>
2024-05-30 10:47:50 +0530

    modal with title and description

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
-const Modal = ({ isOpen, onClose, imageSrc, imageAlt, onNext, onPrevious }) => {
+const Modal = ({ isOpen, onClose, images, currentIndex, onNext, onPrevious }) => {
   const [isClosing, setIsClosing] = useState(false);
   const modalRef = useRef(null);
 
@@ -38,25 +38,29 @@ const Modal = ({ isOpen, onClose, imageSrc, imageAlt, onNext, onPrevious }) => {
 
   return createPortal(
     <div className={`fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-85 ${isClosing ? 'modal-close' : 'modal-open'}`}>
-      <div ref={modalRef} className="relative flex items-center">
+      <div ref={modalRef} className="relative flex flex-col items-center p-2 md:p-4 lg:p-8 max-w-screen-lg w-full">
         <button
           onClick={onPrevious}
-          className="absolute left-2 bg-black bg-opacity-50 hover:bg-opacity-100 w-10 h-10 text-white p-2 rounded-full z-50"
+          className="absolute left-2 border border-[#ffffff40] top-1/2 transform -translate-y-1/2 bg-black bg-opacity-50 hover:bg-opacity-100 w-10 h-10 text-white p-2 rounded-full z-50"
         >
           &larr;
         </button>
-        <div className="relative">
+        <div className="relative bg-black border border-[#ffffff40] p-2 rounded-lg shadow-lg">
           <button
             onClick={handleClose}
-            className="absolute top-0 right-0 m-4 w-10 h-10 bg-black  text-white rounded-full flex items-center justify-center z-50"
+            className="absolute right-2 top-4 transform -translate-x-1/2 m-2 w-10 h-10 bg-black text-white rounded-full flex items-center justify-center z-50"
           >
             &times;
           </button>
-          <img src={imageSrc} alt={imageAlt} className="rounded-lg max-h-screen" style={{ maxWidth: '100%', maxHeight: '100%' }} />
+          <img src={images[currentIndex].src} alt={images[currentIndex].alt} className="rounded-lg max-h-[100vh] object-contain mx-auto" />
+          <div className="mt-2 text-center">
+            <p className="text-lg font-bold">{images[currentIndex].title}</p>
+            <p className="text-sm">{images[currentIndex].subtitle}</p>
+          </div>
         </div>
         <button
           onClick={onNext}
-          className="absolute right-2 bg-black w-10 h-10 bg-opacity-50 hover:bg-opacity-100 text-white p-2 rounded-full z-50"
+          className="absolute border border-[#ffffff40] right-2 top-1/2 transform -translate-y-1/2 bg-black w-10 h-10 bg-opacity-50 hover:bg-opacity-100 text-white p-2 rounded-full z-50"
         >
           &rarr;
         </button>
