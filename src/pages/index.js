b322e3d68fb7cc9b26c58df90240f823a47d67be
rohankokkula <rohankokkula01@gmail.com>
2024-05-30 10:48:04 +0530

    moved banner to a separate page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>{" "}
       <main className="min-h-screen scrollbar-hide">
+        <div className="relative w-full h-[600px] mb-8">
+          <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center text-center p-4">
+            <h1 className="text-4xl sm:text-6xl font-bold text-white mb-4">
+              {" "}
+              Rohan Kokkula{" "}
+            </h1>{" "}
+            <p className="text-xl sm:text-2xl text-white">
+              {" "}
+              My Creative Space{" "}
+            </p>{" "}
+          </div>{" "}
+        </div>{" "}
         <ImageGrid />
       </main>{" "}
     </div>
